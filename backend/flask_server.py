3880b6c7e8aabe50af71afb69f4f189cc060c3c7
shane_william_miller <shanemiller@iinet.net.au>
2025-01-30 22:37:13 +0800

    new commit for bug fixes

--- backend/flask_server.py
+++ backend/flask_server.py
@@ -1,13 +1,21 @@
 import os
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
-from flask_sock import Sock
+from flask_socketio import SocketIO
+import eventlet
+
+eventlet.monkey_patch()
 
 app = Flask(__name__)
 CORS(app)
-sock = Sock(app)
 
-clients = set()
+socketio = SocketIO(
+    app,
+    cors_allowed_origins="*",
+    async_mode='eventlet',
+    ping_timeout=10000,
+    ping_interval=5000
+)
 
 @app.route('/overlay')
 def overlay():
@@ -19,22 +27,9 @@ def handle_bits():
     bits_used = data.get("bits_used", 0)
     user = data.get("user_name", "Unknown")
     
-    # Broadcast to all connected clients
-    for client in clients:
-        try:
-            client.send('show_fire')
-        except:
-            clients.remove(client)
-    
+    socketio.emit('show_fire_gif', {'show': True})
     return jsonify({"status": "success", "message": f"{user} spent {bits_used} Bits!"})
-
-@sock.route('/ws')
-def ws(sock):
-    clients.add(sock)
-    while True:
-        data = sock.receive()
-        print(f"Received websocket message: {data}")
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    socketio.run(app, host="0.0.0.0", port=port)
