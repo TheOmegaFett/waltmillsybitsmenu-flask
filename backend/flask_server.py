1e8d310d4dd44b6ea5e5bbebb1ed356472b13c99
shane_william_miller <shanemiller@iinet.net.au>
2025-01-31 18:40:30 +0800

    Update Redis connection to use Render host

--- backend/flask_server.py
+++ backend/flask_server.py
@@ -4,11 +4,10 @@ eventlet.monkey_patch()
 import redis
 
 redis_client = redis.Redis(
-    host='localhost',  # or your Redis host
-    port=6379,        # default Redis port
-    db=0              # default database
+    host='red-cg2hh0kg1b2c73b2p8p0',  # Your Render Redis host
+    port=6379,
+    db=0
 )
-
 
 
 from flask import Flask, render_template, request, jsonify
