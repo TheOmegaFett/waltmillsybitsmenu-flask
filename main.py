f49554ececdc20415422bc01a54e34140c9417bb
shane_william_miller <shanemiller@iinet.net.au>
2025-02-01 05:00:23 +0800

    Update to protection for drop bear attack

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import random
 class Bot(commands.Bot):
     def __init__(self):
         # Your existing bot initialization code here
-        self.protected_users = set()  # Set to track protected users
-        self.dropbear_active = False  # Flag for active drop bear event
+        self.protected_users = set()
+        self.dropbear_active = False
         
     async def event_ready(self):
         print(f'Logged in as | {self.nick}')
@@ -18,24 +18,28 @@ class Bot(commands.Bot):
             self.protected_users.add(ctx.author.name)
             await ctx.send(f'Fair dinkum! {ctx.author.name} survived the Drop Bear attack!')
             
-    @commands.command(name='dropbear')  # Command to trigger drop bear event
+    @commands.command(name='dropbear')
     async def dropbear_command(self, ctx):
-        if not self.dropbear_active:  # Only start if no active event
-            self.protected_users.clear()  # Clear previous protections
+        if not self.dropbear_active:
+            # Clear at start
+            self.protected_users.clear()
             self.dropbear_active = True
             
-            locations = ['cricket pitch', 'footy oval', 'bush track', 'billabong']
-            items = ['Fairy bread force field', 'Golden Gaytime staff', 'Vegemite shield']
+            locations = ['cricket pitch', 'footy oval', 'bush track', 'billabong', 'Centrelink queue', 'gum tree']
+            items = ['Fairy bread force field', 'Golden Gaytime staff', 'Vegemite shield', 'VB chain mail']
             
             location = random.choice(locations)
             item = random.choice(items)
             
             await ctx.send(f'🐨 STREWTH! Drop Bear spotted near the {location}! Quick, type !protect to use your {item}!')
             
-            # Schedule end of drop bear event
-            await asyncio.sleep(30)
-            self.dropbear_active = False
-            self.protected_users.clear()
+            # Create separate task for ending the event
+            asyncio.create_task(self.end_dropbear_event())
+    
+    async def end_dropbear_event(self):
+        await asyncio.sleep(30)
+        self.dropbear_active = False
+        self.protected_users.clear()  # Clear at end
 
 bot = Bot()
 bot.run()
